d079dd6f9a90c3b66ac47f4d91bc25a9460bb291
Patrick Hanna <patrickjh1998@hotmail.com>
2020-12-23 19:41:33 -0500

    updated / improved menu to be fixed and blurry

--- src/HomePage/IntroSection/NavBar/NavBar.tsx
+++ src/HomePage/IntroSection/NavBar/NavBar.tsx
@@ -18,48 +18,48 @@ function NavBar() {
 
     const [currentSelection, setCurrentSelection] = useState<Optional<Selection>>(null);
 
-    const brocoliImageJsx = (additionalClass?: string) => <img alt="" src={brocoliImage} className={`brocoli-image ${additionalClass ?? ''}`} />;    
-
     return <nav className="NavBar">
-        <div className="logo-and-title-holder">
-            {brocoliImageJsx('left')}
-            <div className="better-living-title">
-                Better Living
+        <div className="content">
+            <div className="logo-and-title-holder">
+                <img alt="" src={brocoliImage} className="brocoli-image" />
+                <div className="better-living-title">
+                    Better Living
+                </div>
             </div>
+            <div className="nav-link-container">
+                {[
+                    Selection.pricing,
+                    Selection.aboutUs,
+                    Selection.testimonials,
+                    Selection.history,
+                    Selection.contactUs,
+                ].map((selection, index) =>
+                    <div
+                        className={[
+                            "nav-link",
+                            ...(currentSelection === selection ? ["selected"] : [])
+                        ].join(' ')}
+                        key={index}
+                        onClick={() => {
+                            setCurrentSelection(selection);
+                        }}
+                    >{(() => {
+                        switch (selection) {
+                            case Selection.pricing: return 'Pricing';
+                            case Selection.aboutUs: return 'About Us';
+                            case Selection.testimonials: return 'Testimonials';
+                            case Selection.history: return 'History';
+                            case Selection.contactUs: return 'Constact Us';
+                        }
+                    })()}</div>
+                )}
+            </div>
+            <MenuSVG className="menu-svg" />
+            <a className="web-app-button" target="_blank" href="https://betterlivingnassau.com" rel="noreferrer">
+                Go to Web App
+            </a>
         </div>
-        <div className="nav-link-container">
-            {[
-                Selection.pricing,
-                Selection.aboutUs,
-                Selection.testimonials,
-                Selection.history,
-                Selection.contactUs,
-            ].map((selection, index) =>
-                <div
-                    className={[
-                        "nav-link",
-                        ...(currentSelection === selection ? ["selected"] : [])
-                    ].join(' ')}
-                    key={index}
-                    onClick={() => {
-                        setCurrentSelection(selection);
-                    }}
-                >{(() => {
-                    switch (selection) {
-                        case Selection.pricing: return 'Pricing';
-                        case Selection.aboutUs: return 'About Us';
-                        case Selection.testimonials: return 'Testimonials';
-                        case Selection.history: return 'History';
-                        case Selection.contactUs: return 'Constact Us';
-                    }
-                })()}</div>
-            )}
-        </div>
-        <MenuSVG className="menu-svg"/>
-        {brocoliImageJsx('center')}
-        <a className="web-app-button" target="_blank" href="https://betterlivingnassau.com" rel="noreferrer">
-            Go to Web App
-        </a>
+
 
     </nav>
 }
