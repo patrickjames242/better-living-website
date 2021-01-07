746e1f9e7e5755d77ebe983af1eeb6d9f317f203
Patrick Hanna <patrickjh1998@hotmail.com>
2021-01-07 18:58:43 -0500

    added map to home page, edited mobile app section

--- src/HomePage/AppAndMapSection/AppAndMapSection.tsx
+++ src/HomePage/AppAndMapSection/AppAndMapSection.tsx
@@ -6,6 +6,7 @@ import phoneImage2 from './appPhoneImage2.png';
 import googlePlayBadge from './google-play-badge.png';
 import appStoreBadge from './app-store-badge.png';
 import { BETTER_LIVING_APP_URL } from '../NavBar/helpers';
+import BetterLivingLocationMap from '../../helpers/BetterLivingLocationMap/BetterLivingLocationMap';
 
 export interface AppAndMapSectionProps {
 
@@ -13,30 +14,42 @@ export interface AppAndMapSectionProps {
 
 function AppAndMapSection(props: AppAndMapSectionProps) {
     return <div className="AppAndMapSection">
-        <div className="content">
-            <div className="left-side">
-                <div className="phone-images-holder">
-                    <img src={phoneImage2} alt="" className="image-1" />
-                    <img src={phoneImage} alt="" className="image-2" />
+        {/* <div className="content"> */}
+            <div className="app-section">
+                <div className="left-side">
+                    <div>
+                        <div>
+                            <div className="phone-images-holder">
+                                <img src={phoneImage2} alt="" className="image-1" />
+                                <img src={phoneImage} alt="" className="image-2" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="text-container right-side">
+                    <div className="title">Download Our Mobile App Today!</div>
+                    <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, maxime esse eaque tempore culpa placeat minus doloremque voluptatibus a! Porro.</div>
+                    <div className="app-store-buttons">
+                        <a href="https://apps.apple.com/us/app/better-living-health-center/id1541047557" target="_blank" rel="noreferrer">
+                            <img src={appStoreBadge} alt="" />
+                        </a>
+                        <a href="https://play.google.com/store/apps/details?id=com.patrickhanna.betterliving" target="_blank" rel="noreferrer">
+                            <img src={googlePlayBadge} alt="" />
+                        </a>
+                    </div>
+                    <a href={BETTER_LIVING_APP_URL} target="_blank" rel="noreferrer" className="web-app-button">
+                        Use Web App
+                    </a>
                 </div>
             </div>
-            <div className="right-side">
-                <div className="title">Download Our Mobile App Today!</div>
-                <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, maxime esse eaque tempore culpa placeat minus doloremque voluptatibus a! Porro.</div>
-                <div className="app-store-buttons">
-                    <a href="https://apps.apple.com/us/app/better-living-health-center/id1541047557" target="_blank" rel="noreferrer">
-                        <img src={appStoreBadge} alt=""/>
-                    </a>
-                    <a href="https://play.google.com/store/apps/details?id=com.patrickhanna.betterliving" target="_blank" rel="noreferrer">
-                        <img src={googlePlayBadge} alt=""/>
-                    </a>
+            <div className="map-section">
+                <div className="text-container">
+                    <div className="title">Come And Visit Us In Person!</div>
+                    <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, maxime esse eaque tempore.</div>
                 </div>
-
-                <a href={BETTER_LIVING_APP_URL} target="_blank" rel="noreferrer" className="web-app-button">
-                    Use Web App
-                </a>
+                <BetterLivingLocationMap/>
             </div>
-        </div>
+        {/* </div> */}
 
     </div>
 }
--- src/helpers/BetterLivingLocationMap/BetterLivingLocationMap.tsx
+++ src/helpers/BetterLivingLocationMap/BetterLivingLocationMap.tsx
@@ -1,10 +1,12 @@
 
-import './ScrewsWorldLocationMap.scss';
+import './BetterLivingLocationMap.scss';
 import React, { useRef, useEffect, useMemo } from 'react';
 import mapboxgl from 'mapbox-gl';
-import backToCenterIcon from '../../HomePage/AppAndMapSection/back-to-center-icon';
+import backToCenterIcon from './back-to-center-icon';
 import { Optional } from '../general';
 mapboxgl.accessToken = 'pk.eyJ1IjoicGF0cmlja2hhbm5hMjQyIiwiYSI6ImNqcnh2eWVrczBydGo0OWx2dDUyYjhvNnMifQ.SGbGDXppFmFkdUnBxIyoqA';
+
+
 
 export default function BetterLivingLocationMap(){
 
@@ -13,7 +15,7 @@ export default function BetterLivingLocationMap(){
     const mapBoxMap = useRef<Optional<mapboxgl.Map>>(null);
 
     const centerCoordinate = useMemo(() => ({ lon: -77.339006, lat: 25.052057 }), []);
-    const defaultZoom = 14.5;
+    const defaultZoom = 13;
     // map is loaded when its section is actually scrolled into view because loading the map is expensive and slows down the page while it is loading
     useEffect(() => {
 
@@ -29,12 +31,12 @@ export default function BetterLivingLocationMap(){
             
             const map = new mapboxgl.Map({
                 container: mapDivID,
-                style: 'https://api.mapbox.com/styles/v1/patrickhanna242/ckjkp1aqh05851ao9nc42wvtr.html?fresh=true&title=view&access_token=pk.eyJ1IjoicGF0cmlja2hhbm5hMjQyIiwiYSI6ImNqcnh2eWVrczBydGo0OWx2dDUyYjhvNnMifQ.SGbGDXppFmFkdUnBxIyoqA',
+                style: 'mapbox://styles/patrickhanna242/ckjkp1aqh05851ao9nc42wvtr',
                 center: centerCoordinate,
                 zoom: defaultZoom,
             });
             mapBoxMap.current = map;
-            const marker = new mapboxgl.Marker({ color: "#0470d9" });
+            const marker = new mapboxgl.Marker({ color: "#27C478" });
             
             marker.setLngLat(centerCoordinate).addTo(map);
             
