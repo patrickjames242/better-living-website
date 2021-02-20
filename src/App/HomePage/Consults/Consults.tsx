86be5bd3497ba10203143198d830ff75445356a4
Patrick Hanna <patrickjh1998@hotmail.com>
2021-02-20 10:30:56 -0500

    finished consults section

--- src/App/HomePage/Consults/Consults.tsx
+++ src/App/HomePage/Consults/Consults.tsx
@@ -2,10 +2,10 @@
 import './Consults.scss';
 import React from 'react';
 import SectionTitleView from '../SectionTitleView/SectionTitleView';
-
 import picture1 from './Picture1.png';
 import picture2 from './Picture2.png';
 import picture3 from './Picture3.png';
+import ChevronSVG from './ChevronSVG';
 
 
 const ImageContainer = (props: { imageURL: string }) => {
@@ -37,15 +37,50 @@ const Consults: React.ForwardRefRenderFunction<HTMLDivElement, {}> = function Co
             title={"We Offer Consults in Health & Wellness"}
             description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero unde doloribus dicta. Delectus neque reiciendis doloribus illum nostrum illo, itaque sed alias. Minima neque voluptate optio, accusantium quidem placeat error unde fugit?"}
         />
-        <div className="corporate-wellness-container">
-            <div className="left-section">
-                <ImageContainer imageURL={picture3} />
-                <ImageContainer imageURL={picture1} />
-                <ImageContainer imageURL={picture2} />
-                <ImageContainer imageURL={picture3} />
+        <div className="content">
+            <div className="corporate-wellness-container">
+                <div className="left-section">
+                    <ImageContainer imageURL={picture3} />
+                    <ImageContainer imageURL={picture1} />
+                    <ImageContainer imageURL={picture2} />
+                    <ImageContainer imageURL={picture3} />
+                </div>
+                <TitleSubtitleContainer title="Coporate, Employee, & Group Wellness Seminars & Programs" description="Employee wellness seminars are very effective in raising the morale of your staff, decreasing absenteeism and the amount of National Insurance claims." />
             </div>
-            <TitleSubtitleContainer title="Coporate, Employee, & Group Wellness Seminars & Programs" description="Employee wellness seminars are very effective in raising the morale of your staff, decreasing absenteeism and the amount of National Insurance claims."/>
+            <div className="personal-wellness-container">
+                <TitleSubtitleContainer title="Dietitian's Consultation Office For Lifestyle Plans" description="Employee wellness seminars are very effective in raising the morale of your staff, decreasing absenteeism and the amount of National Insurance claims." />
+                <div className="column">
+                    {[
+                        'Vegetarian Lifestyle',
+                        'High Cholesterol',
+                        'Diabetes',
+                        'Food Allergies',
+                        'Crohn\'s Disease',
+
+                    ].map((x, index) => (
+                        <div className="item" key={index}>
+                            <ChevronSVG />
+                            <div className="title">{x}</div>
+                        </div>
+                    ))}
+                </div>
+                <div className="column">
+                    {[
+                        'Acid Reflux',
+                        'Arthritis',
+                        'Blood Pressure',
+                        'Yeast Infection',
+                        'Weight Management',
+                    ].map((x, index) => (
+                        <div className="item" key={index}>
+                            <ChevronSVG />
+                            <div className="title">{x}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
+
     </div>
 }
 
